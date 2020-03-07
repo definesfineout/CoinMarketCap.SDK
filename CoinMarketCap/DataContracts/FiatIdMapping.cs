a892ae8922c110b566c8ebcf5475fc448a848b21
Defines Fineout <dustin.fineout@gmail.com>
2020-03-07 17:50:56 -0500

    Implement FiatClient.Map and add to demo (#35)
    Co-authored-by: Defines Fineout <dfineout@silverchair.com>

---
+++ CoinMarketCap/DataContracts/FiatIdMapping.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace CoinMarketCap.DataContracts
+{
+    /// <summary>
+    /// Mapping of Fiat asset to CoinMarketCap ID.
+    /// </summary>
+    [DataContract]
+    public class FiatIdMapping
+    {
+        /// <summary>
+        /// The unique CoinMarketCap ID for this asset.
+        /// </summary>
+        [DataMember(Name="id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of this asset.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The currency sign for this asset.
+        /// </summary>
+        [DataMember(Name = "sign")]
+        public string Sign { get; set; }
+
+        /// <summary>
+        /// The ticker symbol for this asset, always in all caps.
+        /// </summary>
+        [DataMember(Name = "symbol")]
+        public string Symbol { get; set; }
+    }
+}
---
+++ CoinMarketCap/Enumerations/eSortFiatMap.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace CoinMarketCap.Enumerations
+{
+    public enum eSortFiatMap
+    {
+        [Description("id")]
+        Id,
+        [Description("name")]
+        Name
+    }
+}
