bf1bf47946a5d80d39e52c40a670e65a3aa1ac12
M-Fineout <mdfineout@gmail.com>
2020-03-09 00:24:20 +0000

    Temp commits to pull updated master and merge

---
+++ CoinMarketCap/Client/GlobalMetricsClient.cs
@@ -0,0 +1,48 @@
+﻿using CoinMarketCap.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinMarketCap.Client
+{
+    public class GlobalMetricsClient : ApiClientBase
+    {
+        public GlobalMetricsClient(string apiKey, bool sandbox = false)
+            : base(apiKey, sandbox)
+        { }
+
+        #region Endpoint: /v1/global-metrics/quotes/latest - Quotes Latest
+
+        /// <summary>
+        /// Returns the latest global cryptocurrency market metrics. Use the <value>convert</value> option to return market
+        /// values in multiple fiat and cryptocurrency conversions in the same call.
+        /// </summary>
+        /// <param name="convert">
+        /// Optionally calculate market quotes in up to 120 currencies at once by passing a comma-separated list of
+        /// cryptocurrency or fiat currency symbols. Each additional convert option beyond the first requires an additional
+        /// call credit. A list of supported fiat options can be found at 
+        /// https://sandbox.coinmarketcap.com/api/v1/#section/Standards-and-Conventions. 
+        /// Each conversion is returned in its own <value>quote</value> object.
+        /// </param>
+        /// <param name="convertId">
+        /// Optionally calculate market quotes by CoinMarketCap ID instead of symbol. This option is
+        /// identical to <see cref="convert"/> outside of ID format. Ex: <value>convert_id=1,2781</value>
+        /// would replace <value>convert=BTC,USD</value> in your query. This parameter cannot be used when
+        /// <see cref="convert"/> is used.
+        /// </param>
+        /// <returns></returns>
+        public ApiResponse<GlobalMetricsQuotesLatest> QuotesLatest(
+            string convert = null, string convertId = null)
+        {
+            return ApiRequest<ApiResponse<GlobalMetricsQuotesLatest>>("global-metrics/quotes/latest",
+                new Dictionary<string, string>()
+                {
+                    ["convert"] = convert,
+                    ["convert_id"] = convertId
+
+                });
+        }
+
+        #endregion Endpoint: /v1/global-metrics/quotes/latest - Quotes Latest
+    }
+}
---
+++ CoinMarketCap/DataContracts/GlobalMetricsQuotesLatest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CoinMarketCap.DataContracts
+{
+    public class GlobalMetricsQuotesLatest
+    {
+        /// <summary>
+        /// Bitcoin's market dominance percentage by market cap.
+        /// </summary>
+        [DataMember(Name = "btc_dominance")]
+        public double BtcDominance { get; set; }
+
+        /// <summary>
+        /// Ethereum's market dominance percentage by market cap.
+        /// </summary>
+        [DataMember(Name = "eth_dominance")]
+        public double EthDominance { get; set; }
+
+        /// <summary>
+        /// Number of active cryptocurrencies tracked by CoinMarketCap. This includes all cryptocurrencies with a 
+        /// <see cref="listingStatus"/> of <value>active</value> or <value>listed</value> as returned from our 
+        /// <see cref="CryptocurrencyIdMapping" /> call.
+        /// </summary>
+        [DataMember(Name = "active_cryptocurrencies")]
+        public double ActiveCryptocurrencies { get; set; }
+
+        /// <summary>
+        /// Number of active market pairs tracked by CoinMarketCap across all exchanges.
+        /// </summary>
+        [DataMember(Name = "active_market_pairs")]
+        public double ActiveMarketPairs { get; set; }
+
+        /// <summary>
+        /// Number of active exchanges tracked by CoinMarketCap. This includes all exchanges with a 
+        /// <see cref="listingStatus"/> of <value>active</value> or <value>listed</value> as returned by our 
+        /// TODO: <see cref="ExchangeMap"/>/exchange/map call.
+        /// </summary>
+        [DataMember(Name= "active_exchanges")]
+        public double ActiveExchanges { get; set; }
+
+        /// <summary>
+        /// Timestamp of when this record was last updated.
+        /// </summary>
+        [DataMember(Name = "last_updated")]
+        public string LastUpdated { get; set; }
+
+        /// <summary>
+        /// A map of market quotes in different currency conversions.The default map included is <value>USD</value>.
+        /// </summary>
+        [DataMember(Name = "quote")]
+        public Dictionary<string, MarketQuote> MarketQuotes { get; set; }
+    }
+
+    /// <summary>
+    /// A market quote in the specified currency conversion.
+    /// </summary>
+    [DataContract]
+    public class MarketQuote
+    {
+        /// <summary>
+        /// The sum of all individual cryptocurrency market capitalizations in the requested currency.
+        /// </summary>
+        [DataMember(Name = "total_market_cap")]
+        public double TotalMarketCap { get; set; }
+
+        /// <summary>
+        /// The sum of rolling 24 hour adjusted volume TODO: (as outlined in our methodology) for all cryptocurrencies in the
+        /// requested currency.
+        /// </summary>
+        [DataMember(Name = "total_volume_24h")]
+        public double TotalVolume24H { get; set; }
+
+        /// <summary>
+        /// The sum of rolling 24 hour reported volume for all cryptocurrencies in the requested currency.
+        /// </summary>
+        [DataMember(Name = "total_volume_24h_reported")]
+        public double TotalVolume24HReported { get; set; }
+
+        /// <summary>
+        /// The sum of rolling 24 hour adjusted volume TODO: (as outlined in our methodology) for all cryptocurrencies excluding
+        /// Bitcoin in the requested currency.
+        /// </summary>
+        [DataMember(Name = "altcoin_volume_24h")]
+        public double AltcoinVolume24H { get; set; }
+
+        /// <summary>
+        /// The sum of rolling 24 hour reported volume for all cryptocurrencies excluding Bitcoin in the requested currency.
+        /// </summary>
+        [DataMember(Name = "altcoin_volume_24h_reported")]
+        public double AltcoinVolume24HReported { get; set; }
+
+        /// <summary>
+        /// The sum of all individual cryptocurrency market capitalizations excluding Bitcoin in the requested currency.
+        /// </summary>
+        [DataMember(Name = "altcoin_market_cap")]
+        public double AltcoinMarketCap { get; set; }
+
+        /// <summary>
+        /// Timestamp (ISO 8601) of when the conversion currency's current value was referenced.
+        /// </summary>
+        [DataMember(Name = "last_updated")]
+        public string LastUpdated { get; set; }
+    }
+}
